3eca9b1d20d4c6d25f99facd535e94028c95d204
Marco Vela <36453977+MarcoVela@users.noreply.github.com>
2020-04-16 21:02:19 -0500

    all CORS

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import * as bodyParser from 'body-parser';
 import { PORT } from './config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: { origin: false } });
+  const app = await NestFactory.create(AppModule, { cors: {
+      "origin": "*",
+      "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+      "preflightContinue": false,
+      "optionsSuccessStatus": 204
+    } });
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   await app.listen(PORT);
