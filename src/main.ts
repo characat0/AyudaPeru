fcbb2a67b4d5ab99016830f3204a29ab050917a3
Marco Vela <36453977+MarcoVela@users.noreply.github.com>
2020-04-16 20:07:59 -0500

    No CORS

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as bodyParser from 'body-parser';
 import { PORT } from './config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: false });
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   await app.listen(PORT);
