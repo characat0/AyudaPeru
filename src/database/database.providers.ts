4b88ff8d71b50a8dc5929042225d11b0e9321c91
Marco Vela <36453977+MarcoVela@users.noreply.github.com>
2020-05-01 22:17:40 -0500

    ENV

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize-typescript";
 import { sequelizeToken } from "../constants";
-import { databaseConfig } from "../config";
+import { databaseConfig, ENV } from '../config';
 import { Distrito } from './schema/Distrito.model';
 import { PostCode } from './schema/PostCode.model';
 import { Area } from './schema/Area.model';
@@ -55,7 +55,7 @@ Ayuda.belongsTo(Area, { foreignKey: { name: 'areaId' }});
 export const databaseProvider: FactoryProvider = {
   provide: sequelizeToken,
   useFactory: async () => {
-    await sequelize.sync();
+    ENV === "prod" ? await sequelize.authenticate() : await sequelize.sync();
     return sequelize;
   }
 };
